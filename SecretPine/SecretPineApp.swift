4e286362da06fdbde458ad3aad3fcf2f86f57a2d
Denis Sharapov <denisharapov@yandex.ru>
2022-11-16 21:55:42 +0700

    Initial Commit

---
+++ SecretPine/SecretPineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretPineApp.swift
+//  SecretPine
+//
+//  Created by National Team on 16.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SecretPineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
